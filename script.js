d20435bc32f2895ced2978ff5ed87866738c0fb4
kubergl <kubergl@naver.com>
2025-02-25 13:26:00 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+var models = [
+  {
+    url: './assets/assest2.glb',
+    scale: '0.5 0.5 0.5',
+    rotation: '0 180 0',
+  }
+];
+
+var modelIndex = 0;
+var setModel = function (model, entity) {
+  if (model.scale) {
+    entity.setAttribute('scale', model.scale);
+  }
+  if (model.position) {
+    entity.setAttribute('position', model.position);
+  }
+
+  entity.setAttribute('gltf-model', model.url);
+};
+
+function renderPlaces(places) {
+  let scene = document.querySelector('a-scene');
+
+  places.forEach((place) => {
+    let latitude = place.location.lat;
+    let longitude = place.location.lng;
+
+    let model = document.createElement('a-entity');
+    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+    setModel(models[modelIndex], model);
+
+    model.setAttribute('animation-mixer', '');
+    scene.appendChild(model);
+  });
+}
